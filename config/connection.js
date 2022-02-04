78a40c878adb8a04753857983adc5699ad949b40
KiloWattDot <dot.gilchrist95@gmail.com>
2022-02-04 17:53:08 -0500

    Got all gets working for every route but not post/delete/put. I can see the id chosen in delete but won't actually destroy the entry

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,38 @@
 require('dotenv').config();
 const Sequelize = require('sequelize');
 
-let sequelize;
-const URI = process.env.MYSQL_URL || process.env.JAWSDB_URL || 'mysql://root:dorothy@localhost:3306/ecommerce_db';
+// let sequelize;
+// const URI = process.env.MYSQL_URL || process.env.JAWSDB_URL || 'mysql://root:dorothy@localhost:3306/ecommerce_db';
 
-sequelize = new Sequelize(URI)
+// sequelize = new Sequelize(URI)
+
+const sequelize = new Sequelize(
+    // Database name
+    'ecommerce_db',
+    // User
+    'root',
+    // Password
+    'dorothy',
+    {
+      // Database location
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306
+    }
+  );
+
+
+// const sequelize = 
+//     new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+//       host: 'localhost',
+//       dialect: 'mysql',
+//       dialectOptions: {
+//         decimalNumbers: true,
+//       },
+//     });
+
+
+
 
 
 module.exports = sequelize;
--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -11,42 +11,83 @@ router.get('/', async (req, res) => {
   try { 
     const categoryLists = await Category.findAll({
       include:  { model: Product } 
-    })
-      res.status(200).res.json(categoryLists);
+    });
+      res.status(200).json(categoryLists);
   } catch (err) { 
-    res.status(500).json(err)
+    res.status(500).json(err);
   }
+  
+
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
   try { 
     const categoryLists = await Category.findByPk(req.params.id, {
-      include: [{model: Product}], 
+      include: {model: Product}, 
     })
+
     if (categoryLists === null) {
-      console.log('Not found!');
-    } else {
-      console.log(project instanceof Project); // true
-      // Its primary key is 123
-    } catch {
-      
-    }
+      res.status(404).json({ message:'ID Not found!'});
+      return;
+    } 
+
+    res.status(200).json(categoryLists);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.post('/', async (req, res) => {
+  // create a new category.
+  try { 
+    const categoryLists = await Category.create(req.body);
+    res.status(200).json(categoryLists);
+
+  } catch (err) {
+    res.status(400).json({ message:'Please try again!'});;
   }
 
-});
+})
 
-router.post('/', (req, res) => {
-  // create a new category
-});
-
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try {
+    const categoryLists = await Category.update(req.body, { 
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (categoryLists === 0) {
+      res.status(404).json({ message:'ID Not found!'})
+      return
+    }
+    res.status(200).json(categoryLists)
+  } catch (err) { 
+    res.status(500).json(err);
+}
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try {
+    const categoryLists = await Category.destroy({ 
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!categoryLists) {
+      res.status(404).json({ message:'ID Not found!'})
+      return;
+    }
+
+    res.status(200).json(categoryLists)
+  } catch (err) { 
+    res.status(500).json(err);
+}
+
 });
 
 module.exports = router;
